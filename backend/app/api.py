b7ec9a7c15bc0f2b125b4dda742e08a06a359633
CxRxExAxM <mimyers@proton.me>
2025-02-24 18:31:04 -0700

    Added mock JSON source and changed data faile path

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+import json
+import os
+
 
 app = FastAPI()
 
@@ -9,8 +12,14 @@ mock_items = [
     {"id": 3, "name": "Cocktail Shrimp", "outlet": "bar2"}
 ]
 
+def load_items():
+    file_path = os.path.join(os.path.dirname(__file__), '/Users/mikemyers/Library/CloudStorage/ProtonDrive-mimyers@proton.me-folder/Label App/backend/data/items.json')
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
 @app.get('/items')
 def get_items(outlet: str):
+    items = load_items()
     filtered_items = [item for item in mock_items if item['outlet'] == outlet]
     return filtered_items
 
