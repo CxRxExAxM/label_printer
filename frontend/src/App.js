efc6eb5d4aaeb088269db1ca6c467e232f778d93
CxRxExAxM <mimyers@proton.me>
2025-02-24 21:22:19 -0700

    after tailwind test

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-blue-600">Test Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
