68069928a5a4c89650c825b303247639489d1b1a
CxRxExAxM <mimyers@proton.me>
2025-02-24 18:48:49 -0700

    finished FastApi setup and tested mock print

--- backend/app/api.py
+++ backend/app/api.py
@@ -6,11 +6,6 @@ import os
 app = FastAPI()
 
 
-mock_items = [
-    {"id": 1, "name": "Grilled Chicken", "outlet": "cafe1"},
-    {"id": 2, "name": "Beef Stew", "outlet": "cafe1"},
-    {"id": 3, "name": "Cocktail Shrimp", "outlet": "bar2"}
-]
 
 def load_items():
     file_path = os.path.join(os.path.dirname(__file__), '/Users/mikemyers/Library/CloudStorage/ProtonDrive-mimyers@proton.me-folder/Label App/backend/data/items.json')
@@ -20,7 +15,7 @@ def load_items():
 @app.get('/items')
 def get_items(outlet: str):
     items = load_items()
-    filtered_items = [item for item in mock_items if item['outlet'] == outlet]
+    filtered_items = [item for item in items if item['outlet'] == outlet]
     return filtered_items
 
 @app.post('/print-label')
